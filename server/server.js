99710a7bc37eb47c13792ab020a2e40d4ae825ad
jayvardhan <jayvardhanprajapat@gmail.com>
2022-10-03 15:00:43 +0530

    added cors and react

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 4000;
+var cors = require('cors')
+
+
+app.use(express.json());
+app.use(cors())
 
 async function main() {
   try {
@@ -10,10 +15,19 @@ async function main() {
   } catch (error) {}
 }
 
-// main();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+   
+  res.send("hello world");
+  
+});
+
+app.post("/", (req, res) => {
+
+    
+    console.log(req.body.date)
+  res.send(req);
+  
 });
 
 app.listen(PORT, () => console.log(`Server is Listening at ${PORT} `));
