04589dfdc68b47c5baf17bd8eecdd83d03600bb5
jayvardhan <jayvardhanprajapat@gmail.com>
2022-10-03 11:38:23 +0530

    Created  FOLDER structure

---
+++ server/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+
+const app = express()
+const PORT = 4000;
+
+
+app.get('/',(req,res)=>{
+
+    res.send('Hello World')
+
+})
+
+
+app.listen(PORT,()=>console.log(`Server is Listening at ${PORT} `))
