99f4a5bf63ae944f1d4980f482a57b9cb2705ce0
jayvardhan <jayvardhanprajapat@gmail.com>
2022-10-06 17:59:28 +0530

    Fixed for issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import AppBar from "./components/AppBar";
 import TransactionForm from "./components/TransactionForm";
-const Initialform = { amount: 0, description: " ", date: "" };
+
+
+
 
 function App() {
   // created a form state and stored all form data in form object.
-  const [form, setForm] = useState(Initialform);
+ 
 
   const [transactions, setTransactions] = useState([]);
 
@@ -22,51 +24,16 @@ function App() {
     console.log(data);
   }
 
-  function handleInput(e) {
-    // It stores input changes in form object
-    setForm({ ...form, [e.target.name]: e.target.value });
-  }
+
 
  
-  async function handleSubmit(e) {
-    e.preventDefault(); // It prevents default form refresh on submit.
 
-    // It makes an API req to backend, and sends form object in body of request.
-    let res = await fetch("http://localhost:4000/transaction", {
-      method: "POST",
-      body: JSON.stringify(form),
-      headers: {
-        "content-type": "application/json", // by setting this, we now get req.body as json in server.js file.
-      },
-    });
-    if (res.ok) {
-      setForm(Initialform);
-      fetchTransactions();
-    }
-    console.log(res);
-  }
 
   return (
     <div>
       <AppBar />
       <TransactionForm/>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          name="amount"
-          placeholder=" Enter transaction amount "
-          value={form.amount}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
-          name="description"
-          placeholder=" Enter transaction details "
-          onChange={handleInput}
-        />
-        <input type="date" name="date" onChange={handleInput} />
-        <button>Submit</button>
-      </form>
+      
       
       <br></br>
       <br></br>
--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -7,25 +7,48 @@ import TextField from "@mui/material/TextField";
 import { LocalizationProvider, DesktopDatePicker } from "@mui/x-date-pickers/";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-const Initialform = { amount: 0, description: " ", date: "" };
+const Initialform = { amount: 0, description: " ", date: new Date() };
 
 export default function TransactionForm() {
   const [form, setForm] = useState(Initialform);
-  const [transactions, setTransactions] = useState([]);
 
+  function handleChange(e) {
+    // It stores input changes in form object
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
 
+  async function handleSubmit(e) {
+    e.preventDefault(); // It prevents default form refresh on submit.
+
+    // It makes an API req to backend, and sends form object in body of request.
+    let res = await fetch("http://localhost:4000/transaction", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json", // by setting this, we now get req.body as json in server.js file.
+      },
+    });
+    if (res.ok) {
+      setForm(Initialform);
+      //   fetchTransactions();
+    }
+    console.log(res);
+  }
 
   return (
     <Card sx={{ minWidth: 275, marginTop: 10 }}>
       <CardContent>
         <Typography variant="h6">Add New Transaction</Typography>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField
             id="outlined-basic"
             label="Amount"
             variant="outlined"
             sx={{ marginRight: 6 }}
             size="small"
+            name="amount"
+            value={form.amount}
+            onChange={handleChange}
           />
           <TextField
             id="outlined-basic"
@@ -33,13 +56,17 @@ export default function TransactionForm() {
             variant="outlined"
             sx={{ marginRight: 6 }}
             size="small"
+            name="description"
+            value={form.description}
+            onChange={handleChange}
           />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DesktopDatePicker
               label="Transaction Date"
               inputFormat="MM/DD/YYYY"
-              //   value={value}
-            //   onChange={handleChange}
+              value={form.date}
+              name="date"
+              onChange={handleChange}
               renderInput={(params) => (
                 <TextField {...params} sx={{ marginRight: 6 }} size="small" />
               )}
