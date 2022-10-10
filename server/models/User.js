cea9ab545aebe494a2e209bfb23255554aa285f9
jayvardhan <jayvardhanprajapat@gmail.com>
2022-10-10 23:06:06 +0530

    updated User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-  firstname: { type: String, required: ["First name is required"] },
-  lastname: { type: String, required: ["Last name is required"] },
-  email: { type: String, required: ["Email is required"] },
-  password: { type: String, required: ["Password is required"] },
-    },
-{timestamp:true}
-    );
+  {
+    firstname: { type: String, required: ["First name is required"] },
+    lastname: { type: String, required: ["Last name is required"] },
+    email: { type: String, required: ["Email is required"] },
+    password: { type: String, required: ["Password is required"] },
+  },
+  {
+    timestamp: true,
+  }
+);
 
 const User = new mongoose.model("User", UserSchema);
 
